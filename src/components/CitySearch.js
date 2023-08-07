26821e4f3bf47f4aae4076c5ee5ea28e5f760f70
TubaJordan <113389465+TubaJordan@users.noreply.github.com>
2023-08-07 16:38:23 -0700

    connected to api

--- src/components/CitySearch.js
+++ src/components/CitySearch.js
@@ -1,6 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-const CitySearch = ({ allLocations }) => {
+const CitySearch = ({ allLocations, setCurrentCity }) => {
     const [showSuggestions, setShowSuggestions] = useState(false);
     const [query, setQuery] = useState("");
     const [suggestions, setSuggestions] = useState([]);
@@ -19,7 +19,13 @@ const CitySearch = ({ allLocations }) => {
         const value = event.target.textContent;
         setQuery(value);
         setShowSuggestions(false);
+        setCurrentCity(value);
     }
+
+    useEffect(() => {
+        setSuggestions(allLocations);
+    }, [`${allLocations}`]);
+
 
     return (
         <div id="city-search">
@@ -27,7 +33,8 @@ const CitySearch = ({ allLocations }) => {
                 type="text"
                 className="city"
                 placeholder="Search for a city"
-                value={query} onFocus={() => setShowSuggestions(true)}
+                value={query}
+                onFocus={() => setShowSuggestions(true)}
                 onChange={handleInputChanged}
             />
             {showSuggestions ?
